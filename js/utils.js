5d942097e42343c6745fdbf21b3e87a41086943f
Dan Higgins <daniel.higgins@yahoo.com>
2023-05-23 15:35:38 -0400

    Added animation

--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,8 @@
-$(document).ready(function () {
-
+$(document).ready(function(){
+    calc_stats() 
+});
+    
+function calc_stats() {
     $('.counter').each(function () {
         $(this).prop('Counter', 0).animate({
             Counter: $(this).text()
@@ -12,4 +15,44 @@ $(document).ready(function () {
         });
     });
 
-});
+};
+
+/**************************************
+ * WORK IN PROGGRESS
+ 
+$(allInView);
+$(window).scroll(allInView);
+
+
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+function isScrolledIntoView(elem) {
+    console.log(elem)
+    
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+    
+   return false
+}
+
+function allInView() {
+
+    if (isScrolledIntoView($(".walkerlab_facts_container"))) {
+        calc_stats()
+
+    };
+}
+*/
